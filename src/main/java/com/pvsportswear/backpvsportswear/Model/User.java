6ea8c1ad94062a3dec26eddba73ee2fffb004681
Franze18 <132767003+Franze18@users.noreply.github.com>
2024-07-02 15:12:55 +0800

    Update User.java

--- src/main/java/com/pvsportswear/backpvsportswear/Model/User.java
+++ src/main/java/com/pvsportswear/backpvsportswear/Model/User.java
@@ -39,14 +39,9 @@ public class User {
         return password;
     }
 
-    public void setId(Long id){
-        this.id = id;
-    }
-
     public void setName(String name){
         this.name = name;
     }
-
     
     public void setEmail(String email){
         this.email = email;
